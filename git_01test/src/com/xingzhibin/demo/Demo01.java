ddaccde373ddfbdf56c33f3c966bf6cc72b3348b
xing <937394841@qq.com>
2018-12-17 19:04:43 +0800

    git_01test

---
+++ git_01test/src/com/xingzhibin/demo/Demo01.java
@@ -0,0 +1,8 @@
+package com.xingzhibin.demo;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("idea git");
+        System.out.println("hello git");
+    }
+}
